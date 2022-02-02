678f2460e8e1d53838b26ccc8502255a59da5e1f
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-02-02 15:02:19 +0700

    refactor: membuat kode lebih mudah di berikan value

--- src/credentials.js
+++ src/credentials.js
@@ -4,7 +4,32 @@ const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
+const FORM_WRAPPER = process.env.FORM_WRAPPER || "body";
+const INPUT_USERNAME = process.env.INPUT_USERNAME || 'input[type="text"]';
+const INPUT_PASSWORD = process.env.INPUT_PASSWORD || 'input[type="password"]';
+const BUTTON_SUBMIT = process.env.BUTTON_SUBMIT || 'button[type="submit"]';
+
+const SUCCESS_INDICATOR_ELEMENTS = process.env.SUCCESS_INDICATOR_ELEMENTS.split(
+  ","
+).map((el) => el.trim()) || ["body"];
+
 if (!USERNAME || !PASSWORD || !WEBHOOK_URL)
   throw new Error("Seluruh environment variable dibutuhkan!");
 
-module.exports = { USERNAME, PASSWORD, WEBHOOK_URL };
+module.exports = {
+  // Authentication
+  USERNAME,
+  PASSWORD,
+
+  // Discord webhook url
+  WEBHOOK_URL,
+
+  // Login elements
+  FORM_WRAPPER,
+  INPUT_USERNAME,
+  INPUT_PASSWORD,
+  BUTTON_SUBMIT,
+
+  // Success Indicator
+  SUCCESS_INDICATOR_ELEMENTS,
+};
--- src/login.js
+++ src/login.js
@@ -1,30 +1,37 @@
-const { USERNAME, PASSWORD } = require("./credentials");
+const {
+  USERNAME,
+  PASSWORD,
+
+  FORM_WRAPPER,
+  INPUT_USERNAME,
+  INPUT_PASSWORD,
+  BUTTON_SUBMIT,
+
+  SUCCESS_INDICATOR_ELEMENTS,
+} = require("./credentials");
 
 module.exports = (page, infoFN) => async () => {
   infoFN("Mencari dialog login");
   await Promise.all([
-    page.waitForSelector(".auth-form-wrapper"),
-    page.waitForSelector('input[type="text"]'),
-    page.waitForSelector('input[type="password"]'),
-    page.waitForSelector('button[type="submit"]'),
+    page.waitForSelector(FORM_WRAPPER),
+    page.waitForSelector(INPUT_USERNAME),
+    page.waitForSelector(INPUT_PASSWORD),
+    page.waitForSelector(BUTTON_SUBMIT),
   ]);
   infoFN("Dialog login ditemukan, menulis username dan password");
 
-  await page.type('input[type="text"]', USERNAME);
-  await page.type('input[type="password"]', PASSWORD);
+  await page.type(INPUT_USERNAME, USERNAME);
+  await page.type(INPUT_PASSWORD, PASSWORD);
   infoFN("Username dan password selesai di ketik, menekan tombol login");
 
   await Promise.all([
-    page.click('button[type="submit"]'),
+    page.click(BUTTON_SUBMIT),
     page.waitForNavigation({ waitUntil: "networkidle2" }),
   ]);
   infoFN("Berhasil menekan tombol login, mengecek apakah berpindah halaman");
 
-  await Promise.all([
-    page.waitForSelector(".navbar"),
-    page.waitForSelector(".navbar-content"),
-    page.waitForSelector(".sidebar-toggler"),
-    page.waitForSelector(".page-content"),
-  ]);
+  await Promise.all(
+    SUCCESS_INDICATOR_ELEMENTS.map((element) => page.waitForSelector(element))
+  );
   infoFN("Elemen terkonfirmasi bukan halaman login lagi");
 };
